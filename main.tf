9331901d8dca48c3f20c713a30c6e894e097b94b
Aires Januário Mendes <aires.jmendes@gmail.com>
2026-04-16 14:43:21 -0300

    feat: update location to eastus2 and add remote backend

--- main.tf
+++ main.tf
@@ -5,6 +5,13 @@ terraform {
       version = "~> 3.0"
     }
   }
+
+  backend "azurerm" {
+    resource_group_name  = "terraform-state-rg"
+    storage_account_name = "tfstateaires"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
